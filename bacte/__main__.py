4d5ee1382217b9e747a2a3270c373f0f17c77c36
Aman Phatak <aman@zeffo.me>
2025-09-26 15:39:05 +0530

    feat: opan address for render hosting

--- bacte/__main__.py
+++ bacte/__main__.py
@@ -35,6 +35,6 @@ if __name__ == "__main__":
         ),
         logging_config=log_config
     )
-    uvicorn.run("__main__:app", port=8000, log_level="debug")
+    uvicorn.run("__main__:app", host="0.0.0.0", port=8000, log_level="debug")
     
 
